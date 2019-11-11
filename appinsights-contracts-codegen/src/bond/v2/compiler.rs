90bcd07cbc50fbe13fc1334dd048644fd673cb1b
dmolokanov <dmolokanov@users.noreply.github.com>
2019-11-10 22:43:43 -0800

    Start work on data contract builder

--- appinsights-contracts-codegen/src/bond/v2/compiler.rs
+++ appinsights-contracts-codegen/src/bond/v2/compiler.rs
@@ -1,6 +1,7 @@
 use crate::bond::v2::Visitor;
-use crate::bond::{Attribute, Enum, EnumConstant, Parser, Schema, Struct};
+use crate::bond::*;
 use crate::Result;
+use std::collections::HashSet;
 use std::fs;
 use std::path::Path;
 
@@ -40,17 +41,129 @@ impl Visitor for SchemaGenerator {
         self.visit_declarations(schema.declarations());
     }
 
-    //    fn visit_struct(&mut self, declaration: &Struct) {
-    //        unimplemented!()
-    //    }
+    fn visit_struct(&mut self, declaration: &Struct) {
+        // generate struct declaration
+        let mut struct_declaration = codegen::Struct::new(declaration.name());
+
+        let mut struct_generator = StructGenerator::new(&mut struct_declaration);
+        struct_generator.visit_struct(declaration);
+
+        self.module.push_struct(struct_declaration);
+
+        // generate struct builder declaration
+        let builder_name = format!("{}Builder", declaration.name());
+        let mut builder_declaration = codegen::Struct::new(&builder_name);
+        let mut builder_implementation = codegen::Impl::new(&builder_name);
+
+        let mut builder_generator = BuilderGenerator::new(&mut builder_declaration, &mut builder_implementation);
+        builder_generator.visit_struct(declaration);
+
+        self.module.push_struct(builder_declaration);
+        self.module.push_impl(builder_implementation);
+
+        // todo generate implementation of TelemetryData trait
+    }
 
     fn visit_enum(&mut self, declaration: &Enum) {
-        let mut code = codegen::Enum::new(declaration.name());
+        let enumeration = self.module.new_enum(declaration.name());
 
-        let mut generator = EnumGenerator::new(&mut code);
+        let mut generator = EnumGenerator::new(enumeration);
         generator.visit_enum(declaration);
+    }
+}
 
-        self.module.push_enum(code);
+struct StructGenerator<'a> {
+    declaration: &'a mut codegen::Struct,
+    generics: HashSet<&'a str>,
+    field_names: HashSet<String>,
+}
+
+impl<'a> StructGenerator<'a> {
+    fn new(declaration: &'a mut codegen::Struct) -> Self {
+        Self {
+            declaration: declaration.derive("Debug").derive("Serialize").vis("pub"),
+            generics: HashSet::default(),
+            field_names: HashSet::default(),
+        }
+    }
+}
+
+impl Visitor for StructGenerator<'_> {
+    fn visit_struct_attribute(&mut self, attribute: &Attribute) {
+        if attribute.names().iter().any(|name| name == "Description") {
+            self.declaration.doc(attribute.value());
+        }
+    }
+
+    fn visit_field(&mut self, field: &Field) {
+        // skip duplicating fields
+        if self.field_names.insert(field.name()) {
+            // add a new generic parameter to struct declaration
+            if let Some(generic) = field.type_().generic() {
+                if !self.generics.contains(&generic) {
+                    self.declaration.generic(generic);
+                }
+            }
+
+            // add a field declaration to struct
+            let field_type = codegen::Type::from(field.clone());
+            self.declaration.field(&field.name(), &field_type);
+        }
+    }
+}
+
+struct BuilderGenerator<'a> {
+    declaration: &'a mut codegen::Struct,
+    implementation: &'a mut codegen::Impl,
+    constructor: &'a mut codegen::Function,
+    setters: Vec<codegen::Function>,
+    generics: HashSet<&'a str>,
+    field_names: HashSet<String>,
+}
+
+impl<'a> BuilderGenerator<'a> {
+    fn new(declaration: &'a mut codegen::Struct, implementation: &'a mut codegen::Impl) -> Self {
+        let mut constructor = codegen::Function::new("new");
+        implementation.push_fn(constructor);
+        Self {
+            declaration: declaration.vis("pub"),
+            implementation,
+            constructor: &mut constructor,
+            setters: Vec::default(),
+            generics: HashSet::default(),
+            field_names: HashSet::default(),
+        }
+    }
+}
+
+impl Visitor for BuilderGenerator<'_> {
+    fn visit_struct_attribute(&mut self, attribute: &Attribute) {
+        if attribute.names().iter().any(|name| name == "Description") {
+            let doc = format!("Creates an instance of: {}", attribute.value());
+            self.declaration.doc(&doc);
+        }
+    }
+
+    fn visit_field(&mut self, field: &Field) {
+        // skip duplicating fields
+        if self.field_names.insert(field.name()) {
+            if let Some(generic) = field.type_().generic() {
+                // skip duplicating generic parameters
+                if !self.generics.contains(&generic) {
+                    // add a new generic parameter to builder declaration
+                    self.declaration.generic(generic);
+
+                    // add a new generic parameter to builder implementation
+                    self.implementation.generic(generic);
+                }
+            }
+
+            // add a field declaration to builder declaration
+            let field_type = codegen::Type::from(field.clone());
+            self.declaration.field(&field.name(), &field_type);
+
+            //
+        }
     }
 }
 
@@ -60,18 +173,13 @@ struct EnumGenerator<'a> {
 
 impl<'a> EnumGenerator<'a> {
     fn new(code: &'a mut codegen::Enum) -> Self {
-        Self { code }
+        Self {
+            code: code.derive("Debug").derive("Serialize").vis("pub"),
+        }
     }
 }
 
 impl Visitor for EnumGenerator<'_> {
-    fn visit_enum(&mut self, declaration: &Enum) {
-        self.code.derive("Debug").derive("Serialize").vis("pub");
-
-        self.visit_enum_constants(declaration.constants());
-        self.visit_attributes(declaration.attributes());
-    }
-
     fn visit_enum_constant(&mut self, constant: &EnumConstant) {
         self.code.new_variant(constant.name());
 
@@ -80,7 +188,7 @@ impl Visitor for EnumGenerator<'_> {
         }
     }
 
-    fn visit_attribute(&mut self, attribute: &Attribute) {
+    fn visit_enum_attribute(&mut self, attribute: &Attribute) {
         if attribute.names().iter().any(|name| name == "Description") {
             self.code.doc(attribute.value());
         }
---
+++ appinsights-contracts-codegen/src/bond/v2/visitor.rs
@@ -0,0 +1,87 @@
+use crate::bond::*;
+
+pub trait Visitor {
+    fn visit_schema(&mut self, schema: &Schema) {
+        self.visit_declarations(schema.declarations())
+    }
+
+    fn visit_declarations(&mut self, declarations: &Vec<UserType>) {
+        for declaration in declarations {
+            match &declaration {
+                UserType::Struct(declaration) => {
+                    self.visit_struct(declaration);
+                }
+                UserType::Enum(declaration) => {
+                    self.visit_enum(declaration);
+                }
+            };
+        }
+    }
+
+    fn visit_struct(&mut self, declaration: &Struct) {
+        if let Some(base) = declaration.base() {
+            self.visit_base(base);
+        }
+
+        self.visit_struct_attributes(declaration.attributes());
+        self.visit_fields(declaration.fields());
+    }
+
+    fn visit_struct_attributes(&mut self, attributes: &Vec<Attribute>) {
+        for attribute in attributes {
+            self.visit_struct_attribute(attribute);
+        }
+    }
+
+    fn visit_struct_attribute(&mut self, attribute: &Attribute) {}
+
+    fn visit_base(&mut self, declaration: &Type) {
+        if let Type::Complex(ComplexType::User { declaration }) = &declaration {
+            if let UserType::Struct(declaration) = &**declaration {
+                self.visit_struct(declaration);
+            } else {
+                panic!(format!("Unsupported struct base type: {:?}", declaration))
+            }
+        } else {
+            panic!(format!("Unsupported struct base type: {:?}", declaration))
+        }
+    }
+
+    fn visit_fields(&mut self, fields: &Vec<Field>) {
+        for field in fields {
+            self.visit_field(field);
+        }
+    }
+
+    fn visit_field(&mut self, field: &Field) {
+        self.visit_field_attributes(field.attributes());
+    }
+    fn visit_field_attributes(&mut self, attributes: &Vec<Attribute>) {
+        for attribute in attributes {
+            self.visit_field_attribute(attribute);
+        }
+    }
+
+    fn visit_field_attribute(&mut self, attribute: &Attribute) {}
+
+    fn visit_enum(&mut self, declaration: &Enum) {
+        self.visit_enum_constants(declaration.constants());
+        self.visit_enum_attributes(declaration.attributes());
+    }
+
+    fn visit_enum_constants(&mut self, constants: &Vec<EnumConstant>) {
+        for constant in constants {
+            self.visit_enum_constant(constant);
+        }
+    }
+
+    fn visit_enum_constant(&mut self, constant: &EnumConstant) {}
+
+    fn visit_enum_attributes(&mut self, attributes: &Vec<Attribute>) {
+        for attribute in attributes {
+            self.visit_enum_attribute(attribute);
+        }
+    }
+
+    fn visit_enum_attribute(&mut self, attribute: &Attribute) {}
+}
