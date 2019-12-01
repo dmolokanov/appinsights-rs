73ae8a9bef0af8deffcc21fbbb49b2a01824d285
dmolokanov <dmolokanov@users.noreply.github.com>
2019-12-01 12:45:31 -0800

    Code clean up

--- appinsights/tests/client.rs
+++ appinsights/tests/client.rs
@@ -1,45 +1,37 @@
-use appinsights::{TelemetryClient, TelemetryConfig};
-use chrono::{DateTime, Utc};
-use http::{Response, StatusCode};
-use serde_json::json;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
+use appinsights::{TelemetryClient, TelemetryConfig};
+use chrono::{DateTime, Utc};
+use http::{Response, StatusCode};
+use serde_json::json;
+
 #[test]
 fn it_sends_one_telemetry_item() {
-    //    let server = server::respose(compose(200)).create();
-    //    server.
-
-    //    let _e1 = mockito::mock("POST", "track")
-    //        .with_body_from_fn(|w| w.write_all("ok"))
-    //        .with_status()
-    //        .create();
-    //
-    env_logger::builder().filter_level(log::LevelFilter::Debug).init();
-
     let server = server().status(StatusCode::OK).create();
-    dbg!(server.url());
 
     let config = TelemetryConfig::builder()
-        .i_key("key")
+        .i_key("instrumentation key")
         .endpoint(server.url())
         .interval(Duration::from_millis(500))
         .build();
+
     let client = TelemetryClient::from_config(config);
 
     client.track_event("--event--".into());
+
     thread::sleep(Duration::from_secs(3)); // todo use waitgroup instead
     let requests = server.requests.lock().expect("lock read");
+
     assert_eq!(requests.len(), 1)
 }
 
 struct TestServer {
     url: String,
     requests: Arc<Mutex<Vec<String>>>,
-    responses: Arc<Vec<Response<String>>>,
 }
 
 impl TestServer {
@@ -49,15 +41,11 @@ impl TestServer {
 }
 
 struct Builder {
-    requests: Vec<String>,
     responses: Vec<Response<String>>,
 }
 
 fn server() -> Builder {
-    Builder {
-        requests: Vec::new(),
-        responses: Vec::new(),
-    }
+    Builder { responses: Vec::new() }
 }
 
 impl Builder {
@@ -71,13 +59,12 @@ impl Builder {
         }
 
         let response = builder.body(body).unwrap();
-
         self.responses.push(response);
 
         self
     }
 
-    fn status(mut self, status: StatusCode) -> Self {
+    fn status(self, status: StatusCode) -> Self {
         let body = json!({
             "itemsAccepted": 1,
             "itemsReceived": 1,
@@ -91,10 +78,9 @@ impl Builder {
         let (tx, rx) = mpsc::channel();
 
         let requests = Arc::new(Mutex::new(Vec::new()));
-        let requests_copy = Arc::clone(&requests);
+        let requests_copy = requests.clone();
 
-        let responses = Arc::new(self.responses);
-        let responses_copy = Arc::clone(&responses);
+        let mut responses = self.responses.into_iter();
 
         thread::spawn(move || {
             let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
@@ -124,16 +110,14 @@ impl Builder {
                             }
 
                             let chunk = String::from_utf8_lossy(&buffer[..bytes]);
-                            dbg!(&chunk);
                             body.push_str(&chunk);
                         }
 
                         let mut requests = requests_copy.lock().expect("lock");
                         requests.push(body);
 
-                        if let Some(response) = responses_copy.first() {
+                        if let Some(response) = responses.next() {
                             let line = format!("HTTP/1.1 {}\r\n\r\n", response.status());
-                            dbg!(&line);
                             stream.write_all(line.as_bytes()).unwrap();
                         } else {
                             let line = "HTTP/1.0 404 Not Found";
@@ -150,10 +134,7 @@ impl Builder {
         });
 
         let url = rx.recv().ok().and_then(|url| url).unwrap();
-        TestServer {
-            url,
-            requests,
-            responses,
-        }
+
+        TestServer { url, requests }
     }
 }
