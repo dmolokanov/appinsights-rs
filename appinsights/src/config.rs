aa2ba2fd98692d07d1949f9545a60f59408bdba1
Thomas Eckert <thomas.james.eckert@gmail.com>
2020-01-21 18:26:34 -0800

    Small changes made while exploring (#1)
    * Fix some mistypes and errors in comments

--- appinsights/src/config.rs
+++ appinsights/src/config.rs
@@ -11,7 +11,7 @@ use std::time::Duration;
 /// let config = TelemetryConfig::new("<instrumentation key>".to_string());
 /// ```
 ///
-/// Creating a telemetry client configuration with customg settings
+/// Creating a telemetry client configuration with custom settings
 /// ```rust
 /// # use std::time::Duration;
 /// # use appinsights::TelemetryConfig;
@@ -28,7 +28,7 @@ pub struct TelemetryConfig {
     /// Endpoint URL where data will be sent.
     endpoint: String,
 
-    // Maximum time to wait until send a batch of telemetry.
+    /// Maximum time to wait until send a batch of telemetry.
     interval: Duration,
 }
 
